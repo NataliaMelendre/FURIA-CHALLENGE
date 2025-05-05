3808fc7b17abcfe4df4370c5e6f67813867ee960
NataliaMelendre <natmelendreads@gmail.com>
2025-05-04 21:16:10 -0300

    atualizar a url do websocket

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -76,7 +76,7 @@ function handleLogin(event) {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("https://furia-challenge.onrender.com")
     websocket.onmessage = processMessage
 
 }
